aa9aca353d44b8b01fd38bc68ed600f4de01758e
Zhenya <e.mezhueva@gmail.com>
2021-01-15 16:43:53 +0200

    Updates

---
+++ precache-manifest.7f03ba2279b60ce25d19601b763951e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b0641fb4d9638fef25f892f1b5be7b3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5feca9cc81f84af6eed1",
+    "url": "./static/css/main.42ba144b.chunk.css"
+  },
+  {
+    "revision": "9bfabb54970d8a1edebf",
+    "url": "./static/js/2.c17dec48.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.c17dec48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5feca9cc81f84af6eed1",
+    "url": "./static/js/main.a717b11c.chunk.js"
+  },
+  {
+    "revision": "89de80f3985b25002b5f",
+    "url": "./static/js/runtime-main.17b9eccd.js"
+  }
+]);
